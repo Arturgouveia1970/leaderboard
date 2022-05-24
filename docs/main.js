2062f99b67afa6f3d3b553528397920634029a13
Arturgouveia1970 <artgouveiag@gmail.com>
2022-05-24 13:54:46 +0100

    added eslintignore

--- docs/main.js
+++ docs/main.js
@@ -1,18 +1,16 @@
-(self["webpackChunkleaderboard"] = self["webpackChunkleaderboard"] || []).push([["main"],{
+(self.webpackChunkleaderboard = self.webpackChunkleaderboard || []).push([['main'], {
 
-/***/ "./src/index.js":
-/*!**********************!*\
+  /***/ './src/index.js':
+  /*! **********************!*\
   !*** ./src/index.js ***!
-  \**********************/
-/***/ (() => {
+  \********************* */
+  /***/ (() => {
 
-
-
-/***/ })
+    /***/ }),
 
 },
-/******/ __webpack_require__ => { // webpackRuntimeModules
-/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
-/******/ }
+/** *** */ (__webpack_require__) => { // webpackRuntimeModules
+/** *** */ const __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId));
+  /** *** */ const __webpack_exports__ = (__webpack_exec__('./src/index.js'));
+/** *** */ },
 ]);
