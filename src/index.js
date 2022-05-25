e1d8a89f0a0e17cfb3976a365e3c1d6696555a4e
Arturgouveia1970 <artgouveiag@gmail.com>
2022-05-25 18:19:42 +0100

    added API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './styles.css';
+import { addScore, getScore } from './modules/leaderboard.js';
+
+const form = document.querySelector('.form');
+const refresh = document.querySelector('.refresh-button');
+
+refresh.addEventListener('click', getScore);
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const nameInput = form.names.value;
+  const scoreInput = form.marks.value;
+  const userscore = { user: `${nameInput}`, score: `${scoreInput}` };
+
+  addScore(userscore).then(() => {
+    getScore();
+  });
+});
--- src/modules/leaderboard.js
+++ src/modules/leaderboard.js
@@ -1,4 +1,4 @@
-const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/'; 
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/FRZdMMyF1kjqYVxv98wr/scores/'; 
 const scoreTable = document.querySelector('.score-table');
 
 const addScore = async (userscore) => {
@@ -12,11 +12,12 @@ const addScore = async (userscore) => {
   return request.json();
 };
 
-// scoreTable.innerHTML = '';
+scoreTable.innerHTML = '';
 
 const getScore = async () => {
   const request = await fetch(url);
   const data = await request.json();
+
   scoreTable.innerHTML = data.result
     .map((score) => `<li>${score.user}: ${score.score}</li>`)
     .join('');
